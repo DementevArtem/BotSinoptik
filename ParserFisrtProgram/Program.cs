cc81a6b49a7f74a90e616321058af530ed48eec5
Dementev_Artem <artemdementev561@gmail.com>
2021-06-13 15:20:18 +0300

    Add pars in this project

--- ParserFisrtProgram/Program.cs
+++ ParserFisrtProgram/Program.cs
@@ -1,39 +1,16 @@
 ﻿using AngleSharp;
-using AngleSharp.Dom;
-using AngleSharp.Html.Parser;
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Net;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Xml;
-using System.Xml.XPath;
 
 namespace ParserFisrtProgram
 {
     class Program
     {
-
         static async System.Threading.Tasks.Task Main(string[] args)
         {
-            var config = Configuration.Default.WithDefaultLoader();
-            // Устанавливаем адрес страницы сайта
-            var address = "http://page.if.ua/article/6/";
-            // загружаем страницу и разбираем её
-            var document = await BrowsingContext.New(config).OpenAsync(address);
-            // Используем CSS селектор для получения строк таблицы с классом  и выбрать из этой строки 3 колонку 
-            var cellSelector = "table.tbl_afisha td:nth-child(4)";
-            // Получим все ячейки
-            var cells = document.QuerySelectorAll(cellSelector);
-            //Выделим из ячеек текстовое содержимое
-            var titles = cells.Select(m => m.TextContent);
-
-            foreach (var item in titles)
-            {
-                Console.WriteLine(item);
-            }
+            var address = "https://ua.sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D1%96%D0%B2%D0%B0%D0%BD%D0%BE-%D1%84%D1%80%D0%B0%D0%BD%D0%BA%D1%96%D0%B2%D1%81%D1%8C%D0%BA";
+            var serchData = new SearchData();
+            await serchData.GetData(address);
         }
     }
 }
---
+++ ParserFisrtProgram/SearchData.cs
@@ -0,0 +1,23 @@
+﻿using AngleSharp;
+using System;
+using System.Linq;
+
+namespace ParserFisrtProgram
+{
+    class SearchData
+    {
+        internal async System.Threading.Tasks.Task GetData(string linkOfSite)
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            var document = await BrowsingContext.New(config).OpenAsync(linkOfSite);
+            var cellSelector = "div.main.loaded";
+            var cells = document.QuerySelectorAll(cellSelector);
+            var titles = cells.Select(m => m.TextContent);
+
+            foreach (var item in titles)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
